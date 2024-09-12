e9b04d06d74100438d3e6f8e55d1e04e14466072
Chege2004 <chegemanasseh2@gmail.com>
2024-09-12 20:28:07 +0300

    Added remaining inks for navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/signup">Signup</Link></li>
-          <li><Link to="/products">Products</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/products">Products</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/checkout">Checkout</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/orders">Orders</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </ul>
+    </nav>
   );
 };
 
