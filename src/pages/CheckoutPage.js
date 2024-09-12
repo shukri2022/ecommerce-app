debdcec8fb939de812f770ecc9acf3da8c873eaa
Chege2004 <chegemanasseh2@gmail.com>
2024-09-12 20:28:47 +0300

    Added the files for checkout order and profile

---
+++ src/pages/CheckoutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CheckoutPage = () => {
+    return (
+        <div>
+            <h1>Checkout</h1>
+            {/* Add Checkout functionality here */}
+        </div>
+    );
+};
+
+export default CheckoutPage;
