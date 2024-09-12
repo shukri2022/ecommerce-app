0be58acbec9d9418690497886633966e3391da68
Chege2004 <chegemanasseh2@gmail.com>
2024-09-12 20:27:48 +0300

    Added the other files for the 8 routes ie profile,checkout and orders

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage.js';
+import ProfilePage from './pages/Profile.js';
+import OrderPage from './pages/Order.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -17,7 +21,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/products" element={<ProductPage />} />
-        <Route path="/cart" element={<CartPage />} />
+        <Route path="/cart" element={<PrivateRoute><CartPage /></PrivateRoute>} />
+        <Route path="/checkout" element={<PrivateRoute><CheckoutPage /></PrivateRoute>} />
+        <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+        <Route path="/orders" element={<PrivateRoute><OrderPage /></PrivateRoute>} />
       </Routes>
       <Footer />
     </Router>
