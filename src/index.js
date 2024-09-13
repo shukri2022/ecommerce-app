0894c310dcb75a2ae1691c42f40b8509774d1d3b
Chege2004 <chegemanasseh2@gmail.com>
2024-09-13 12:03:44 +0300

    Added the context for the cart

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext';  // Import AuthProvider
+import { CartProvider } from './context/CartContext';  // Import CartProvider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider> {/* Wrap the entire app in AuthProvider */}
-      <App />
+      <CartProvider> {/* Wrap the app in CartProvider */}
+        <App />
+      </CartProvider>
     </AuthProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
