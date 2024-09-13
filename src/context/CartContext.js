5087a76d61fc1416db656ea159da7ed2035f8e74
Chege2004 <chegemanasseh2@gmail.com>
2024-09-13 12:03:17 +0300

    Added the cart context in the for handling items in the cart

---
+++ src/context/CartContext.js
@@ -0,0 +1,24 @@
+// src/context/CartContext.js
+import React, { createContext, useState, useContext } from 'react';
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+    const [cartItems, setCartItems] = useState([]);
+
+    const addToCart = (product) => {
+    setCartItems((prevItems) => [...prevItems, product]);
+    };
+
+    const removeFromCart = (productId) => {
+        setCartItems((prevItems) => prevItems.filter(item => item.id !== productId));
+    };
+
+    return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+        {children}
+    </CartContext.Provider>
+    );
+};
+
+export const useCart = () => useContext(CartContext);
